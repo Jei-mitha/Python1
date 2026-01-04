e6996da9c1fe00bded248d029c7d5f9dd71f1da0
Jei-mitha <jrjeimitha@gmail.com>
2026-01-04 21:06:36 +0530

    Add files via upload

---
+++ pattern code2.py
@@ -0,0 +1,10 @@
+x=int(input("enter number"))
+for i in range(x):
+        print("* ",end='')
+
+for j in range((x-2)):
+                print("\n* "+"  "*(x-2) +"*")   
+        
+for i in range(x):
+              
+       print("* ",end='')
