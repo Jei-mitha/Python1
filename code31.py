06def3bdfbdb0ae0cfd2dca4b269dea25cc632cb
Jei-mitha <jrjeimitha@gmail.com>
2026-03-10 22:40:08 +0530

    Add files via upload

---
+++ code31.py
@@ -0,0 +1,8 @@
+l="python programming is an easy programming method"
+print(l[::-1])
+print(l[2:15])
+print(l[3:16])
+l=["good","python","morning","whispers","magic wand"]
+print(l[::-1])
+print(l[-2:15])
+print(l[2:-2])
